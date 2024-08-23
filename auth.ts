8f8b6a0d683d9f8724e75e428d98664a9d08f73d
tswix <tswekemisc@gmail.com>
2024-08-23 13:30:35 +0200

    commented cookies code out in auth.ts

--- auth.ts
+++ auth.ts
@@ -53,16 +53,16 @@ export const {
     })
   ],
   // Add the following configuration
-  useSecureCookies: process.env.NODE_ENV === 'production',
-  cookies: {
-    pkceCodeVerifier: {
-      name: 'next-auth.pkce.code_verifier',
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production'
-      }
-    }
-  }
+  //useSecureCookies: process.env.NODE_ENV === 'production',
+  //cookies: {
+    //pkceCodeVerifier: {
+      //name: 'next-auth.pkce.code_verifier',
+      //options: {
+        //httpOnly: true,
+        //sameSite: 'lax',
+        //path: '/',
+        //secure: process.env.NODE_ENV === 'production'
+      //}
+    //}
+  //}
 })
