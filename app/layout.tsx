76ccfdd5c560a9c1d646e02fe05293d7c6d442e0
tswix <tswekemisc@gmail.com>
2024-09-27 00:41:05 +0200

    updated root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,15 @@
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 
-import '@/app/globals.css'
-import { cn } from '@/lib/utils'
-import { TailwindIndicator } from '@/components/tailwind-indicator'
-import { Providers } from '@/components/providers'
-import { Header } from '@/components/header'
-import { Toaster } from '@/components/ui/sonner'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
-import Script from 'next/script'
-import { CSPostHogProvider } from './providers'
-
-
+import '@/app/globals.css';
+import { cn } from '@/lib/utils';
+import { TailwindIndicator } from '@/components/tailwind-indicator';
+import { Providers } from '@/components/providers';
+import { Header } from '@/components/header';
+import { Toaster } from '@/components/ui/sonner';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { CSPostHogProvider } from './providers';
 
 export const metadata = {
   metadataBase: process.env.VERCEL_URL
@@ -28,50 +25,47 @@ export const metadata = {
     shortcut: '/favicon-32x32.png',
     apple: '/apple-icon.png'
   }
-}
+};
 
 export const viewport = {
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: 'white' },
     { media: '(prefers-color-scheme: dark)', color: 'black' }
   ]
-}
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  
   return (
     <html lang="en" suppressHydrationWarning>
       <CSPostHogProvider>
-        <body>{children}</body>
-      </CSPostHogProvider>
-      <body
-        className={cn(
-          'font-sans antialiased',
-          GeistSans.variable,
-          GeistMono.variable
-        )}
-      >
-        <Toaster position="top-center" />
-        <Providers
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+        <body
+          className={cn(
+            'font-sans antialiased',
+            GeistSans.variable,
+            GeistMono.variable
+          )}
         >
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
-          </div>
-          <TailwindIndicator />
-        </Providers>
-        <Analytics />
-        <SpeedInsights />
-        
-      </body>
+          <Toaster position="top-center" />
+          <Providers
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="flex flex-col min-h-screen">
+              <Header />
+              <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
+            </div>
+            <TailwindIndicator />
+          </Providers>
+          <Analytics />
+          <SpeedInsights />
+        </body>
+      </CSPostHogProvider>
     </html>
-  )
+  );
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,27 +1,16 @@
 // app/providers.js
 'use client'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
-import { TooltipProvider } from '@radix-ui/react-tooltip'
-import { ReactNode } from 'react'
+import React from 'react';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? '', {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? '',
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || '',
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-  })
+  });
 }
 
-interface CSPostHogProviderProps {
-  children: ReactNode
-}
-
-export function CSPostHogProvider({ children }: CSPostHogProviderProps) {
-  return (
-    <PostHogProvider client={posthog}>
-      <TooltipProvider>
-        {children}
-      </TooltipProvider>
-    </PostHogProvider>
-  )
+export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
