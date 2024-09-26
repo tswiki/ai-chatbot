02a94de5299267d941c490d76de049a1f8259577
tswix <tswekemisc@gmail.com>
2024-09-26 23:54:57 +0200

    posthog integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ import { Toaster } from '@/components/ui/sonner'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 import Script from 'next/script'
+import { CSPostHogProvider } from './providers'
+
 
 
 export const metadata = {
@@ -43,15 +45,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
   
   return (
     <html lang="en" suppressHydrationWarning>
-        <head>
-        {/* Other head elements */}
-        <Script id="heap-analytics" strategy="afterInteractive">
-          {`
-            window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
-            heap.load("138551708");
-          `}
-        </Script>
-      </head>
+        <CSPostHogProvider>
+        <body>{children}</body>
+        </CSPostHogProvider>
       <body
         className={cn(
           'font-sans antialiased',
